0bb6fedb2733352b4f274259f8eb1e6f5443e4cf
FakeDazon <gamerpenguin2005@gmail.com>
2019-10-15 15:34:01 -0700

    fixed it
    if statement was setting value instead of acting as a conditional
    
    lack of one =

--- js/RSL.js
+++ js/RSL.js
@@ -26,9 +26,10 @@ function gotData(data) {
         var code = scores[k].code;
         //console.log(longUrl + " : " + code);
         //console.log(code);
-        if (code = "owo") {
-            document.getElementById('code').textContent = i;
-            gotCode = true;
+        var codeToGet = getQueryVariable("l");
+        if (code == codeToGet) {
+            document.getElementById('code').textContent = code;
+            document.getElementById('lUrl').textContent = longUrl;
             //console.log(gotCode);
         }
     }
@@ -37,4 +38,13 @@ function gotData(data) {
 function errData(err) {
     console.log(err);
     console.log("Error!");
+}
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) { return pair[1]; }
+    }
+    return (false);
 }
