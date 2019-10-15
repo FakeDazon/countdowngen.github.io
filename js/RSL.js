2cac98c611f90e268dd3cf8cc0ea782d1a592ac4
FakeDazon <gamerpenguin2005@gmail.com>
2019-10-15 15:19:08 -0700

    attempted to fix the short link reader; might have found the cause

--- js/RSL.js
+++ js/RSL.js
@@ -7,34 +7,34 @@ var firebaseConfig = {
     storageBucket: "countdowngenshortlinks.appspot.com",
     messagingSenderId: "313544877504",
     appId: "1:313544877504:web:c4652cb6620228e5809425"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  console.log(firebase);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+console.log(firebase);
 
-  var database = firebase.database();
-  var ref = database.ref('links');
-  ref.on('value', gotData, errData);
-  
-  
-  
-  function gotData(data){
-      var scores = data.val();
-      var keys = Object.keys(scores);
-      for (let i = 0; i < keys.length; i++) {
-          var k = keys[i];
-          var longUrl = scores[k].lUrl;
-          var code = scores[k].code;
-          console.log(longUrl + " : " + code);
-          if(code = "eQ1u6"){
-              var codeVal = database.ref('links/' + code + '/lUrl/')
-            document.getElementById('code').textContent = code;
-          }
-          
-      }
-  }
-  
-  function errData(err) {
-      console.log(err);
-      console.log('Error!');
-  }
+var database = firebase.database();
+var ref = database.ref("links");
+ref.on("value", gotData, errData);
+
+function gotData(data) {
+    var scores = data.val();
+    var keys = Object.keys(scores);
+    console.log(scores.code);
+    for (let i = 0; i <= keys.length + 1; i++) {
+        var k = keys[i];
+        var longUrl = scores[k].lUrl;
+        var code = scores[k].code;
+        //console.log(longUrl + " : " + code);
+        //console.log(code);
+        if (code = "owo") {
+            document.getElementById('code').textContent = i;
+            gotCode = true;
+            //console.log(gotCode);
+        }
+    }
+}
+
+function errData(err) {
+    console.log(err);
+    console.log("Error!");
+}
